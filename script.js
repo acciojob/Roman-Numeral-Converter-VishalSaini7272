3b138afa9151c4eda87641e73b7990885cbe8627
VishalSaini7272 <sainivishal156@gmail.com>
2025-08-04 10:37:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,8 +8,18 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
+	
   //your code here
+let roman = '';
+
+  for (let [symbol, value] of romanPairs) {
+    while (num >= value) {
+      roman += symbol;
+      num -= value;
+    }
+  }
+
+  return roman;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
